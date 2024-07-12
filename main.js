4236525e3d44378e56ca17c4c01b0ad5602bd120
dixxyy <saputradicky481@gmail.com>
2024-07-12 23:29:20 +0700

    ngerapihin

--- main.js
+++ main.js
@@ -11,3 +11,19 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
+
+$(document).ready(function () {
+  $(window).on('scroll', function () {
+    var scrollPos = $(document).scrollTop();
+    $('.nav-link').each(function () {
+      var currLink = $(this);
+      var refElement = $(currLink.attr('href'));
+      if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+        $('.nav-link').removeClass('active');
+        currLink.addClass('active');
+      } else {
+        currLink.removeClass('active');
+      }
+    });
+  });
+});
